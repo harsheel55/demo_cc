983deaaa3e6a05fcd587b414080d8847e36acb2e
harsheel55 <22it055@charusat.edu.in>
2025-03-15 21:42:53 +0530

    updated code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders deployment test message', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Test Deployment - March 14, 2025/i);
+  const headerElement = screen.getByText(/Test Deployment - March 14/i);
   expect(headerElement).toBeInTheDocument();
 });
